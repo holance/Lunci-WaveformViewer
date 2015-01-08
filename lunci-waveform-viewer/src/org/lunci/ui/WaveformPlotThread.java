bf8161165cc4bb32ce46ede90782cd68be3100f1
holance <holance@gmail.com>
2015-01-07 23:49:58 -0600

    License Added

--- lunci-waveform-viewer/src/org/lunci/ui/WaveformPlotThread.java
+++ lunci-waveform-viewer/src/org/lunci/ui/WaveformPlotThread.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014 Lunci Hua
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.lunci.ui;
 
 import java.util.concurrent.SynchronousQueue;
@@ -191,7 +207,7 @@ public class WaveformPlotThread extends Thread {
 	public void setConfig(WaveformView.Config config) {
 		if (this.isAlive()) {
 			Message.obtain(mHandler, MESSAGE_SET_CONFIG, 0, 0, config)
-					.sendToTarget();
+			.sendToTarget();
 		} else {
 			updateConfig(config);
 		}
