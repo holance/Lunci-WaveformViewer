cd888b820ec36a04a9f9a21f8301b01c6dfa4fb6
holance <holance@gmail.com>
2015-01-11 01:16:50 -0600

    Separate waveform config into its own class

---
+++ lunci-waveform-viewer/src/org/lunci/waveform/ui/WaveformViewConfig.java
@@ -0,0 +1,16 @@
+package org.lunci.waveform.ui;
+
+public class WaveformViewConfig {
+	public int DataMaxValue = Integer.MAX_VALUE;
+	public int DataMinValue = Integer.MIN_VALUE;
+	public int BackgroundColor = 0xFF000000;
+	public int LineColor = 0xFF00FF00;
+	public int AxisColor = 0xFFFFFFFF;
+	public int PlotThreadPriority = Thread.NORM_PRIORITY + 1;
+	public float VerticalZoom = 1;
+	public float HorizontalZoom = 1;
+	public boolean AutoPositionAfterZoom = false;
+	public int DefaultDataBufferSize = 1000;
+	public int DrawingDeltaX = 8;
+	public boolean ShowFPS = true;
+}
