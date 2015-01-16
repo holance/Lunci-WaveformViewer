40665b5ffaf056d328226514e8b6f8997c93c2cc
Lunci Hua <holance@gmail.com>
2015-01-16 00:20:10 -0600

    Update

---
+++ lunci-waveform-viewer/src/org/lunci/waveform/ui/WaveformGridViewConfig.java
@@ -0,0 +1,13 @@
+package org.lunci.waveform.ui;
+
+import android.graphics.Color;
+
+public class WaveformGridViewConfig {
+	public int DataMaxValue = Integer.MAX_VALUE;
+	public int DataMinValue = Integer.MIN_VALUE;
+	public int NumColumns = 5;
+	public int NumRows = 5;
+	public int AxialXColor = 0xA003A9F4;
+	public int AxialYColor = 0xA003A9F4;
+	public int BackgroundColor = Color.BLACK;
+}
